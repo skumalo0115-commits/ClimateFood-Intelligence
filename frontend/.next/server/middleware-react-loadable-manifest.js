c2e2c2295af47482078187c01824e4080e180427
Sbahle Theo <s.kumalo0115@gmail.com>
2026-03-12 02:53:56 +0200

    Fix Next.js vuln and ignore build artifacts

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/MapPanel\":{\"id\":\"app\\\\page.tsx -> @/components/MapPanel\",\"files\":[\"static/css/_app-pages-browser_components_MapPanel_tsx.css\",\"static/chunks/_app-pages-browser_components_MapPanel_tsx.js\"]}}"
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"znuSC5Oj2Zp7NzjXqqHkX65Oe6Xo/pr+8AffsOZO+y8=\"\n}"
